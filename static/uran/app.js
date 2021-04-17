2387c0bb8e428d42397ee3e3b0a1c6ca5161ee7e
iyozh <evzhenko1106@gmail.com>
2021-04-17 19:48:03 +0300

    initial commit

---
+++ static/uran/app.js
@@ -0,0 +1,17 @@
+$('.menu_btn').on('click', function(e){
+	e.preventDefault;
+	$(this).toggleClass('menu_btn_active');
+	$('.slide_menu').toggleClass('slide_menu_active');
+	$('.nav_menu').toggleClass('nav_menu_active');
+	$('.poloska1').toggleClass('poloska_active');
+	$('.poloska2').toggleClass('poloska_active');
+	$('.poloska3').toggleClass('poloska_active');
+});
+$('.menu_link_btn').on('click', function(e){
+	e.preventDefault;
+	$('.planet_list').toggleClass('planet_list_active');
+	$('.arrow').toggleClass('arrow_active');
+	$('.poloska_active').toggleClass('poloska_act');
+	$('.poloska_active').toggleClass('poloska_act');
+	$('.poloska_active').toggleClass('poloska_act');
+});
